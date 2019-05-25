b528f8c9a2ae6f904a83e2f88d5061aeced300ab
KhallilB <khallil.bailey@gmail.com>
2019-05-25 14:35:10 -0700

    connected to mLabs

--- sever.js
+++ sever.js
@@ -1,5 +1,35 @@
+// Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const server = express();
+
+// Middleware
+server.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+server.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').MONGO_URI;
+
+// Connecting to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('CONNECTED TO DATABASE'))
+  .catch(err => {
+    console.log(`Caught Error: ${err}`);
+    console.log(db);
+  });
+
+// Setting Port
+const port = process.env.PORT || 5000;
+
+// Listening
+server.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
