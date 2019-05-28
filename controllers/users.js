4a9a8ff974d36a55a2510676f2f7f58dae7ce3b1
KhallilB <khallil.bailey@gmail.com>
2019-05-28 15:01:25 -0700

    [start] user controller signup

---
+++ controllers/users.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const passport = require('passport');
+const keys = require('../config/keys');
+
+const User = require('../models/User');
+
+module.exports.signUp = (req, res) => {};
